f255fa279587ff5d89d36229d236d2384fa98d00
Raducu Ileana <Elena.Raducu@exesoftware.ro>
2026-08-07 13:57:36 +0300

    refactor: best practices - reorganize project structure - add folder for service interfaces

---
+++ MeetingPlanner/MeetingPlanner/Services/Contracts/IMeetingService.cs
@@ -0,0 +1,21 @@
+﻿using MeetingPlanner.Models;
+
+namespace MeetingPlanner.Services.Contracts
+{
+    public interface IMeetingService
+    {
+        Task<List<Project>> GetProjectsAsync(); 
+        Task<List<Meeting>> GetMeetingsByProjectAsync(int projectId);
+        Task<Meeting?> GetMeetingByIdAsync(int id);
+        Task AddMeetingAsync(Meeting meeting); 
+        Task UpdateMeetingAsync(Meeting meeting);
+        Task DeteleMeetingAsync(int id);
+
+        Task<List<Meeting>> GetMeetingsAsync(); 
+        Task<Project?> GetProjectByIdAsync(int id);
+
+        int GetLastMeetingID();
+
+        Task<List<MeetingSummary>> GetMeetingSummaries(); 
+    }
+}
