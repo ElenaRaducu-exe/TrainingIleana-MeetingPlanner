5d935131312a63791bfbf8390225f8bce3dcde88
Raducu Ileana <Elena.Raducu@exesoftware.ro>
2026-08-11 16:22:00 +0300

    refactor: dependency injection - best practices

--- MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
+++ MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9894b7687e20369b6b948045c54e6e17a54daa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd8b52b02dada65bbe29001af18c20f8ecf3e88")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
