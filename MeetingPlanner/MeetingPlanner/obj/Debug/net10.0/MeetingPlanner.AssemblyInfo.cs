0f9894b7687e20369b6b948045c54e6e17a54daa
Raducu Ileana <Elena.Raducu@exesoftware.ro>
2026-08-11 15:58:23 +0300

    fix: add await operator to call method

--- MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
+++ MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f333f2383e6a946cc66ebe6c2d25f9e0d03e7f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84d6fdec71f5261373ee4208d95458a0ccf45c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
