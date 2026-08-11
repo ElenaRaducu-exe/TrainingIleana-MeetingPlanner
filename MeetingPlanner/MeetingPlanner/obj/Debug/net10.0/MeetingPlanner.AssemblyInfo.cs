0f333f2383e6a946cc66ebe6c2d25f9e0d03e7f9
Raducu Ileana <Elena.Raducu@exesoftware.ro>
2026-08-11 13:26:01 +0300

    feat: remove cancel button from the dialog

--- MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
+++ MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b9803440eaa42008726caca52ccd08bc4ff3e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6da323362dbc27efac38b0e0b235a5f8ff27315")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
