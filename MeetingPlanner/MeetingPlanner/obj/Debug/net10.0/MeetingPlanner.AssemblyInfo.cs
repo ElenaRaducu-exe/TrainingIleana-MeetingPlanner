74934b2d0ecc010e65c55b2ddf297c991301d5b8
Raducu Ileana <Elena.Raducu@exesoftware.ro>
2026-08-12 15:04:45 +0300

    refactor: best practices - nameof

--- MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
+++ MeetingPlanner/MeetingPlanner/obj/Debug/net10.0/MeetingPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7229c94ed0cf7015f45e6e25a36b65f8ca92689e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93949a578825944821167abf924ca5917c693b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
